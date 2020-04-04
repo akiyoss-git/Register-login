1e86323bd11d7ab527d3f87af80e74ded2113175
akiyoss-git <58162197+akiyoss-git@users.noreply.github.com>
2020-04-04 16:55:22 +0300

    регистрация, прайслист
    Добавлен прайс лист с добавлением, изменением и удалением

--- cpanel/cpanel/urls.py
+++ cpanel/cpanel/urls.py
@@ -1,27 +1,34 @@
 """cpanel URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
-from . import views
 
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.signIn),
-    path('postsign/',views.postsign),
-    path('logout/',views.logout,name="log"),
-    path('signup/',views.signUp,name='signup'),
-    path('postsignup/',views.postsignup,name='postsignup'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$',views.signIn),
+    url(r'^postsign/',views.postsign),
+    url(r'^logout/',views.logout,name="log"),
+    url(r'^signup/',views.signUp,name='signup'),
+    url(r'^postsignup/',views.postsignup,name='postsignup'),
+    url(r'^create/',views.create,name='create'),
+    url(r'^post_create/',views.post_create,name='post_create'),
+    url(r'^check/',views.check,name='check'),
+    url(r'^post_check/',views.post_check,name='post_check'),
+    url(r'^delete/',views.delete,name='delete'),
+    url(r'^change/',views.change,name='change'),
+    url(r'^post_change/',views.post_change,name='post_change'),
 ]
--- cpanel/cpanel/views.py
+++ cpanel/cpanel/views.py
@@ -1,8 +1,6 @@
-import pyrebase 
-from django.shortcuts import render 
+from django.shortcuts import render
+import pyrebase
 from django.contrib import auth
-import random
-#from sender import EmailSender
 
 config = {
   'apiKey': "AIzaSyBXgvtowzljY_2UjRnO4BeRY1CXP55jrXk",
@@ -16,27 +14,25 @@ config = {
 }
 
 firebase = pyrebase.initialize_app(config)
-#sender = EmailSender()
+
 authe = firebase.auth()
 database=firebase.database()
 
 def signIn(request):
     return render(request, "signIn.html")
 
-def postsign(request, email=None, passw=None):
-    if email is None and passw is None:
-        email=request.POST.get('email')
-        passw = request.POST.get("pass")
+def postsign(request):
+    email=request.POST.get('email')
+    passw = request.POST.get("pass")
     try:
         user = authe.sign_in_with_email_and_password(email,passw)
     except:
         message="invalid credentials"
         return render(request,"signIn.html",{"messg":message})
+    print(user['idToken'])
     session_id=user['idToken']
     request.session['uid']=str(session_id)
-    data = database.child("users").child(user['localId']).child("details").child("verified").shallow().get().val()
-    print(data)
-    return render(request, "welcome.html",{"e":email, "verified": 0})
+    return render(request, "welcome.html",{"e":email})
 
 def logout(request):
     auth.logout(request)
@@ -47,28 +43,171 @@ def signUp(request):
     return render(request,"signup.html")
 
 def postsignup(request):
-    phone=request.POST.get('phone')
+    name=request.POST.get('name')
     email=request.POST.get('email')
-    name = request.POST.get('name')
     passw=request.POST.get('pass')
     try:
         user=authe.create_user_with_email_and_password(email,passw)
-        uid = user['localId']
-        data={"name":name, "phone":phone,"verified":"0", "email":email}
-        database.child("users").child(uid).child("details").set(data)
-        return postsign(request, email, passw)
     except:
         message="Unable to create account try again"
         return render(request,"signup.html",{"messg":message})
+        uid = user['localId']
 
-def sendcode(request):
+    data={"name":name,"status":"1"}
+
+    database.child("users").child("details").set(data)
+    return render(request,"signIn.html")
+
+def create(request):
+    return render(request,'create.html',{'state':0})
+
+def check(request):
     print(request)
-    code = random.randint(1000, 9999)
-    #data = database.child("users").child(user['localId']).child("details").child("email").shallow().get().val()
-    #sender.sendmsg()
-    return render(request, "welcome.html")
+    if request.method == 'GET' and 'csrfmiddlewaretoken' in request.GET:
+        search = request.GET.get('search')
+        search = search.lower()
+        uid = request.GET.get('uid')
+        print(search)
+        print(uid)
+        timestamps = database.child('reports').shallow().get().val()
+        price_id=[]
+        for i in timestamps:
 
-def verify(request):
-    return render(request, "welcome.html")
-        
+            wor = database.child('reports').child(i).child('price').get().val()
+            wor = str(wor)+"$"+str(i)
+            price_id.append(wor)
 
+        matching = [str(string) for string in price_id if search in string.lower()]
+
+        s_price=[]
+        s_id=[]
+
+        for i in matching:
+
+            price,ids=i.split('$')
+            s_price.append(price)
+            s_id.append(ids)
+        print(s_price)
+        print(s_id)
+        date = []
+        import datetime
+        for i in s_id:
+            i = float(i)
+            dat = datetime.datetime.fromtimestamp(i).strftime('%H:%M %d-%m-%Y')
+            date.append(dat)
+        comb_lis = zip(s_id, date, s_price)
+        name = database.child('users').child('details').child('name').get().val()
+
+        return render(request, 'check.html', {'comb_lis': comb_lis, 'e': name, 'uid': uid})
+
+    else:
+        import datetime
+        idtoken = request.session['uid']
+        a = authe.get_account_info(idtoken)
+        a = a['users']
+        a = a[0]
+        a = a['localId']
+
+        timestamps = database.child('reports').shallow().get().val()
+        lis_time=[]
+        for i in timestamps:
+
+            lis_time.append(i)
+
+        lis_time.sort(reverse=True)
+
+        print(lis_time)
+        price = []
+        name = []
+        for i in lis_time:
+
+            wor=database.child('reports').child(i).child('price').get().val()
+            nam = database.child('reports').child(i).child('name').get().val()
+            name.append(nam)
+            price.append(wor)
+        print(price)
+
+        comb_lis = zip(lis_time,name,price)
+        username = database.child('users').child('details').child('name').get().val()
+
+        return render(request,'check.html',{'comb_lis':comb_lis,'e':username,'uid':a})
+
+def post_create(request):
+    
+    import time
+    from datetime import datetime, timezone
+    import pytz
+    tz= pytz.timezone('Asia/Kolkata')
+    time_now= datetime.now(timezone.utc).astimezone(tz)
+    millis = int(time.mktime(time_now.timetuple()))
+    print("mili"+str(millis))
+    price = request.POST.get('price')
+    desc =request.POST.get('desc')
+    url = request.POST.get('url')
+    name = request.POST.get('name')
+    idtoken= request.session['uid']
+    a = authe.get_account_info(idtoken)
+    a = a['users']
+    a = a[0]
+    a = a['localId']
+    data = {
+        "name": name,
+        "price":price,
+        'desc':desc,
+        'url':url
+    }
+    database.child('reports').child(millis).set(data)
+    name = database.child('users').child('details').child('name').get().val()
+    return check(request)
+
+def post_check(request):
+    import datetime
+
+    time = request.GET.get('z')
+
+    idtoken = request.session['uid']
+    a = authe.get_account_info(idtoken)
+    a = a['users']
+    a = a[0]
+    a = a['localId']
+
+    wname =database.child('reports').child(time).child('name').get().val()
+    price =database.child('reports').child(time).child('price').get().val()
+    desc =database.child('reports').child(time).child('desc').get().val()
+    name = database.child('users').child('details').child('name').get().val()
+    return render(request,'post_check.html',{'price':price,'desc':desc,'name':wname,'e':name, 't':time})
+
+def delete(request):
+    time = request.POST.get('time')
+    database.child('reports').child(time).remove()
+    return check(request)
+
+def change(request):
+    time = request.POST.get('time')
+    name = request.POST.get('name')
+    desc = request.POST.get('desc')
+    price = request.POST.get('price')
+    print(time)
+    print(price)
+    print(desc)
+    print(name)
+    return render(request,'create.html', {
+        'name':name,
+        'price':price,
+        'desc':desc,
+        't':time,
+        'state':1
+    })
+
+def post_change(request):
+    time = request.POST.get('time')
+    name = request.POST.get('name')
+    desc = request.POST.get('desc')
+    price = request.POST.get('price')
+    data = {
+        'name':name,
+        'desc':desc,
+        'price':price
+    }
+    database.child('reports').child(time).set(data)
+    return check(request)
